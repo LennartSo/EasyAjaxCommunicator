63da26051a9a82fd099ffa79f30c0b8ccabb3ae4
Lennart Sommerfeld <ls@lennart-sommerfeld.de>
2014-11-15 19:34:13 +0100

    Client Script

---
+++ js/easyPK.js
@@ -0,0 +1,29 @@
+// eDataT (Easy PHP Kommunikation)
+
+
+XMLHttpRequest.prototype.targetFunction = '';
+XMLHttpRequest.prototype.targetFunctionPara = '';
+
+XMLHttpRequest.prototype.compileData = function () {
+
+    var functionObject = {};
+    functionObject[this.targetFunction] = [];
+
+    var functionParameter = this.targetFunctionPara;
+
+    for (var i = 0; i < functionParameter.length; i++) {
+        functionObject[this.targetFunction][i] = functionParameter[i];
+    }
+
+    return encodeURIComponent(JSON.stringify(functionObject));
+
+};
+
+XMLHttpRequest.prototype.sendData = function(target){
+    this.open("POST",target);
+    this.setRequestHeader("Content-type","application/x-www-form-urlencoded; charset=utf-8");
+
+    var senDataString = "EasyAjaxJSONString="+this.compileData();
+
+    this.send(senDataString);
+};
